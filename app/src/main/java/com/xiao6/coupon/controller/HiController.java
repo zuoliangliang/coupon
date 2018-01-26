5d185568ae586825edaedb83defbb4e5aa49a5be
zuolianglinag <zuoliangliang@outlook.com>
2018-01-26 11:29:42 +0800

    #swagger2 提供接口说明

--- app/src/main/java/com/xiao6/coupon/controller/HiController.java
+++ app/src/main/java/com/xiao6/coupon/controller/HiController.java
@@ -2,6 +2,7 @@ package com.xiao6.coupon.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 public class HiController {
 
 
-    @RequestMapping("/hi/{name}")
+    @RequestMapping(value = "/hi/{name}" ,method = RequestMethod.POST)
     public String sayHi(@PathVariable String name, HttpServletRequest request) {
         return "From tomcat localPort:"+ request.getLocalPort()+" hi," + name;
     }
--- app/src/main/java/com/xiao6/coupon/controller/UserController.java
+++ app/src/main/java/com/xiao6/coupon/controller/UserController.java
@@ -2,20 +2,32 @@ package com.xiao6.coupon.controller;
 
 import com.xiao6.coupon.mapper.model.User;
 import com.xiao6.coupon.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
+@Api("用户信息相关信息处理")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/user/add/{userName}/{age}")
+    @ApiOperation(value = "添加用户信息", notes = "添加用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName", required = true, dataType = "String", paramType = "path"),
+            @ApiImplicitParam(name = "age", required = true, dataType = "int", paramType = "path")
+    })
+    @RequestMapping(value = "/user/add/{userName}/{age}", method = RequestMethod.GET)
     public int add(@PathVariable String userName, @PathVariable int age) {
         User user = new User();
         user.setAge(age);
@@ -23,6 +35,7 @@ public class UserController {
         return userService.save(user);
     }
 
+    @ApiIgnore
     @RequestMapping("/user/list")
     public List<User> list() {
         return userService.list();
